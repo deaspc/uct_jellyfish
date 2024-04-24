9532c8bf7782fa2936579fd0647e79ee31f9a8df
supicha tunpoon <deadeaspcha@gmail.com>
2024-04-24 13:56:48 +0700

    first

---
+++ s1.js
@@ -0,0 +1,68 @@
+const tl1 = gsap.timeline({
+  scrollTrigger: {
+    markers: false,
+    trigger: ".container1",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+    pinSpacing: false,
+  },
+});
+
+tl1.fromTo(
+  "#text11",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 2,
+  }
+);
+
+tl1.fromTo(
+  "#text11",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    rotateX: 90,
+    duration: 2,
+  }
+);
+
+tl1.fromTo(
+  "#text12",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 2,
+  }
+);
+
+tl1.fromTo(
+  "#text12",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    rotateX: 90,
+    duration: 2,
+  }
+);
+
+tl1.fromTo(
+  "#bg1",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    duration: 2,
+  }
+);
