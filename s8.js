d04d02c4e14f01796a7035a6d2cb1ef44ec464fa
supicha tunpoon <deadeaspcha@gmail.com>
2024-04-29 12:44:16 +0700

    update s8

---
+++ s8.js
@@ -0,0 +1,11 @@
+const tl8 = gsap.timeline({
+  scrollTrigger: {
+    markers: false,
+    trigger: ".container8",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+    pinSpacing: false,
+  },
+});
