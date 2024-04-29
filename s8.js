4a11a803c9bb5a5c9fdd6f6d2918147a3fdb66c4
supicha tunpoon <deadeaspcha@gmail.com>
2024-04-29 13:43:13 +0700

    s8

--- s8.js
+++ s8.js
@@ -9,3 +9,57 @@ const tl8 = gsap.timeline({
     pinSpacing: false,
   },
 });
+
+tl8.fromTo(
+  "#bg8",
+  {
+    filter: "brightness(0)",
+  },
+  {
+    filter: "brightness(1)",
+    duration: 2,
+  }
+);
+
+tl8.fromTo(
+  "#text80",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 2,
+  },
+  "<+=0.1"
+);
+
+tl8.fromTo(
+  "#text80",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    rotateX: 90,
+    duration: 2,
+  }
+);
+
+// 10m
+tl8.fromTo(
+  "#img81l",
+  {
+    opacity: 0,
+    x: 0,
+  },
+  {
+    opacity: 1,
+    x: -1000,
+    duration: 3,
+  },
+  ">+=0.1"
+);
+
+// 20m
+
+// 30m
