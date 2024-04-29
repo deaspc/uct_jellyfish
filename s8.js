a359aae3972857db1f181b55b580b9b36b36aeda
supicha tunpoon <deadeaspcha@gmail.com>
2024-04-29 14:43:11 +0700

    s8

--- s8.js
+++ s8.js
@@ -90,8 +90,7 @@ tl8.fromTo(
     opacity: 1,
     x: 10,
     duration: 3,
-  },
-  "<+=0.1"
+  }
 );
 
 tl8.fromTo(
@@ -120,5 +119,176 @@ tl8.fromTo(
 );
 
 // 20m
+tl8.fromTo(
+  "#bg8",
+  {
+    filter: "brightness(1)",
+  },
+  {
+    filter: "brightness(0.75)",
+    duration: 2,
+  }
+);
+
+tl8.fromTo(
+  "#twenty",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 2,
+  }
+);
+
+tl8.fromTo(
+  "#img82r",
+  {
+    opacity: 0,
+    x: 0,
+  },
+  {
+    opacity: 1,
+    x: 10,
+    duration: 3,
+  }
+);
+
+tl8.fromTo(
+  "#img81l",
+  {
+    opacity: 0,
+    x: 0,
+  },
+  {
+    opacity: 1,
+    x: -10,
+    duration: 3,
+  }
+);
+
+tl8.fromTo(
+  "#twenty",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    duration: 2,
+  },
+  "<+=0.1"
+);
+
+tl8.fromTo(
+  "#img82r",
+  {
+    opacity: 1,
+    x: 10,
+  },
+  {
+    opacity: 0,
+    x: 1000,
+    duration: 5,
+  },
+  "<+=0.1"
+);
+
+tl8.fromTo(
+  "#img81l",
+  {
+    opacity: 1,
+    x: -10,
+  },
+  {
+    opacity: 0,
+    x: -1000,
+    duration: 5,
+  }
+);
 
 // 30m
+tl8.fromTo(
+  "#bg8",
+  {
+    filter: "brightness(0.75)",
+  },
+  {
+    filter: "brightness(0.5)",
+    duration: 2,
+  }
+);
+
+tl8.fromTo(
+  "#thirty",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 2,
+  }
+);
+
+tl8.fromTo(
+  "#img82l",
+  {
+    opacity: 0,
+    x: 0,
+  },
+  {
+    opacity: 1,
+    x: -10,
+    duration: 3,
+  }
+);
+
+tl8.fromTo(
+  "#img83r",
+  {
+    opacity: 0,
+    x: 0,
+  },
+  {
+    opacity: 1,
+    x: 10,
+    duration: 3,
+  }
+);
+
+tl8.fromTo(
+  "#thirty",
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+    duration: 2,
+  }
+);
+
+tl8.fromTo(
+  "#img82l",
+  {
+    opacity: 1,
+    x: -10,
+  },
+  {
+    opacity: 0,
+    x: -1000,
+    duration: 5,
+  }
+);
+
+tl8.fromTo(
+  "#img83r",
+  {
+    opacity: 1,
+    x: 10,
+  },
+  {
+    opacity: 0,
+    x: 1000,
+    duration: 5,
+  },
+  "<+=0.1"
+);
