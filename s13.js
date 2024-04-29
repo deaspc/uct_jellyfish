0e0ced4f9b652893c8f8c9a7cbc20acb94d1764d
supicha tunpoon <deadeaspcha@gmail.com>
2024-04-30 00:03:09 +0700

    s13

---
+++ s13.js
@@ -0,0 +1,11 @@
+const tl13 = gsap.timeline({
+  scrollTrigger: {
+    markers: false,
+    trigger: ".container13",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+    pinSpacing: false,
+  },
+});
