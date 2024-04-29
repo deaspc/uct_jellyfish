24927f1f83601bbb90ea8933cfe2005d9f17a305
supicha tunpoon <deadeaspcha@gmail.com>
2024-04-29 15:27:13 +0700

    s9

---
+++ s9.js
@@ -0,0 +1,11 @@
+const tl9 = gsap.timeline({
+  scrollTrigger: {
+    markers: false,
+    trigger: ".container9",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+    pinSpacing: false,
+  },
+});
