cf715d303237b1db13a69162ca1e653c55cff2c7
supicha tunpoon <deadeaspcha@gmail.com>
2024-04-29 11:56:46 +0700

    updatex

---
+++ s7.js
@@ -0,0 +1,11 @@
+const tl7 = gsap.timeline({
+  scrollTrigger: {
+    markers: false,
+    trigger: ".container7",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+    pinSpacing: false,
+  },
+});
